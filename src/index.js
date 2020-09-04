9cef1fef4896b511e6fae2ff07795b4b5168a5a1
Thomas Devos <devos.thom@gmail.com>
2020-09-05 00:47:19 +0200

    add Webpack, bootstrap and more

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./style.scss"
+import "popper.js/dist/popper"
+import "bootstrap/js/dist/dropdown"
+
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+    entry: './src/index.js',
+    watch: true,
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
+
+module.exports = config;
